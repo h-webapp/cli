dba1509521f42dc957d96362fbf8bc7fd1cc3ec9
koujp <koujp@qq.com>
2018-01-10 21:29:50 +0800

    remove test code

--- vue-template/src/app/test/js/test.js
+++
@@ -1,6 +0,0 @@
-import { Application } from 'webapp-core';
-Application.app('test-app').service('caseService',function () {
-    this.getCase = function () {
-        console.log('test');
-    }
-});
